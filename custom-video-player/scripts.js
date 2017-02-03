da50c82c997abfb10ddb3b0ddffb6705028b9303
AndersonBrantes <andersonbrantes@gmail.com>
2017-02-02 23:58:04 -0200

    Iniciando projeto para estudo de estilização de player de video. / Adicionando seletores dos elementos.

---
+++ custom-video-player/scripts.js
@@ -0,0 +1,10 @@
+// Selecionando os elementos
+
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipbuttons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
