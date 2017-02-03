b69cea09c85386b2ba3bdb8286de853696fa0f96
AndersonBrantes <andersonbrantes@gmail.com>
2017-02-03 00:13:34 -0200

    Adicionando função que dá start e pausa o video.

--- custom-video-player/scripts.js
+++ custom-video-player/scripts.js
@@ -4,7 +4,27 @@ const player = document.querySelector('.player');
 const video = document.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipbuttons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+
+// Funções
+
+function togglePlay() {
+
+/*    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }*/
+
+    // refatorando
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+// Event Listners
+
+video.addEventListener('click', togglePlay);
