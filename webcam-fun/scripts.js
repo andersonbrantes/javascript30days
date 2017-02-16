9dffd9bdedca63f2c1d637d2c5774caf247edcc8
AndersonBrantes <andersonbrantes@gmail.com>
2017-02-15 22:17:29 -0200

    Criando função que tira snapshots do video da camera e printa no elemento canvas a cada 16 segundos.

--- webcam-fun/scripts.js
+++ webcam-fun/scripts.js
@@ -17,4 +17,16 @@ function getVideo() {
         });
 }
 
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(function() {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
 getVideo();
