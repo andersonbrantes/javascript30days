5bf020db7e0cbf0a37f2f07a90c680c8de83287a
AndersonBrantes <andersonbrantes@gmail.com>
2017-02-15 22:00:12 -0200

    Criando função para capturar video da webcam e enviar a image para o elemento video da pagina.

--- webcam-fun/scripts.js
+++ webcam-fun/scripts.js
@@ -3,3 +3,18 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function(localMediaStream) {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(function(err) {
+            console.log("Oh NO!!!", err);
+        });
+}
+
+getVideo();
