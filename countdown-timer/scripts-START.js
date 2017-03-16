db1267163f65681ac6573184148468d5566dd7be
AndersonBrantes <andersonbrantes@gmail.com>
2017-03-16 08:14:30 -0300

    Adicionando funções para itens do menu do topo manipularem o timer.

--- countdown-timer/scripts-START.js
+++ countdown-timer/scripts-START.js
@@ -1,10 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -29,3 +35,29 @@ function displayTimeLeft(seconds) {
     document.title = display;
     timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    endTime.textContent = 'Be back at ' + adjustedHour + ':' + (minutes < 10 ? '0' : '') + minutes;
+}
+
+function startTimer() {
+    var newThis = this;
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', startTimer);
+});
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
